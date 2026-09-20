cddec883b2246e58aebaf8340fb60be63ee21712
Eduardo Kainan <leite_kaynan@hotmail.com>
2026-09-20 11:55:44 -0300

    fix: support nested blog routes on production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante que os caminhos funcionem na hospedagem compartilhada
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
